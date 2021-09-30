6fa8eaa9f9d23f5435cbb8555308cde0cbc5e7a0
李敖 <2931558140@qq.com>
2021-09-30 13:57:31 +0800

    增加setState更新

--- src/cssDemos/index.tsx
+++ src/cssDemos/index.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect } from "react"
 import ReactDOM from "react-dom"
 import { SwordContainer } from "./swords"
 import { Revolution } from "./revolution"
@@ -9,22 +9,66 @@ import {
   Width4Container,
 } from "./width"
 
-const App = () => {
-  return (
-    <div
-      style={{
-        width: "100%",
-        height: "100%",
-      }}
-    >
-      {/* <SwordContainer /> */}
-      {/* <Revolution /> */}
-      <Width1Container />
-      <Width2Container />
-      <Width3Container />
-      <Width4Container />
-    </div>
-  )
+class App extends React.Component<any, any> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      val: 0
+    };
+  }
+
+  componentDidMount() {
+    // 对象更新state会被合并
+    this.setState({ val: this.state.val + 1 },
+      () => {
+        console.log(this.state.val);
+      });
+    this.setState({ val: this.state.val + 1 },
+      () => {
+        console.log(this.state.val);
+      });
+
+    setTimeout(() => {
+      // 函数更新state不会
+      // 但是异步对象更新不会被合并
+      this.setState(
+        { val: this.state.val + 1 },
+        // (preState: any) => ({ val: preState.val + 1 }),
+        () => {
+        }
+      );
+      console.log(this.state.val);
+      this.setState(
+        { val: this.state.val + 1 },
+        // (preState: any) => ({ val: preState.val + 1 }),
+        () => {
+          // console.log(this.state.val);
+        }
+      );
+      console.log(this.state.val);
+    }, 0);
+    console.log(this.state.val);
+  }
+  render() {
+    return (
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        <p>
+          {this.state.val}
+        </p>
+        {/* <SwordContainer /> */}
+        {/* <Revolution /> */}
+        <Width1Container />
+        <Width2Container />
+        <Width3Container />
+        <Width4Container />
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById("root"))
